e2e5760658d09653f8b63f1620850a6111e12f74
tamnan <pryanikov1980@gmail.com>
2022-01-16 16:41:19 +0700

    make turnover trading solusdt

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/Users/artem/PycharmProjects/huobi_turnover/venv/bin/python3
 from datetime import datetime
 import time
 import csv
@@ -11,7 +12,7 @@ from huobi import HuobiRestClient
 current_path = os.path.dirname(__file__)
 abs_path = os.path.abspath(current_path)
 
-USDT_MIN_BALANCE_TO_TRADE = 10
+USDT_MIN_BALANCE_TO_TRADE = 15
 TRADE_FEES_PERCENTAGE = 0.2
 SOL_MIN_BALANCE_TO_TRADE = 0.1
 TARGET_TURNOVER=120
@@ -19,9 +20,11 @@ TARGET_TURNOVER=120
 parser = argparse.ArgumentParser('batch trading huobi accounts')
 parser.add_argument("-v","--verbose", help="level of verbosing ('debug'-print all messages, 'warning'-only important, 'none'-no printing", choices=['debug', 'warning', 'critical', 'd', 'w'],default='warning')
 parser.add_argument("-t","--target_turnover", help="turnover to get on the accounts", type=int, default=TARGET_TURNOVER)
+parser.add_argument('-p', "--price_of_solana", help='current price of solana -5$', type=float, default=125)
 # parser.add_argement("-a",
 args = parser.parse_args()
 TARGET_TURNOVER = args.target_turnover
+current_solana_price = args.price_of_solana
 
 # create our own logger
 main = logging.getLogger('main')
@@ -122,6 +125,18 @@ def sell_sol(client, account_id, sol_balance):
     # print(result.data)
     return amount
 
+def buy_sol(client, account_id, sol_balance):
+    amount = get_trade_volume_including_fees(sol_balance,2)
+    # print('sol amount to trade: ', amount)
+    symbol = 'solusdt'
+    order_type = 'buy-market'
+    result = client.place(account_id=spot_account_id, amount=amount,symbol=symbol, type=order_type)
+    trade_status = result.data['status']
+    if trade_status!="ok":
+        amount = 0
+    # print(result.data)
+    return amount
+
 def sell_usdc(client, account_id, usdt_balance):
     symbol = 'usdcusdt'
     amount = get_trade_volume_including_fees(usdt_balance,4)
@@ -185,68 +200,12 @@ for index, wallet_key_pair in enumerate(tqdm(keys_list_splitted[:])):
         continue
 
     main.debug(f'wallet -{acc_nickname}- has spot account id: {spot_account_id}')
-    
-    while True:
-        
-        error_counter = 0
-        assets_list = get_assets_list(client, spot_account_id)
-        usdt_balance, usdc_balance, sol_balance = parse_assets_list(assets_list)
-        
-        item = get_usdt_usdc_sol_balances(client, spot_account_id, acc_nickname)
-        main.debug(f'initial wallet state (balances) {acc_nickname}: '+str(item))
-        # print(item)
-        
-        if sol_balance>SOL_MIN_BALANCE_TO_TRADE:
-            main.debug(f'wallet {acc_nickname} has sol, start sell it for usdt')
-            volume = sell_sol(client, spot_account_id, sol_balance)
-            if volume == 0:
-                error_counter +=1
-                continue
-            actual_turnover += volume
-            trades_counter +=1
-            global_trades_counter+=1
-            time.sleep(1)
-            item = get_usdt_usdc_sol_balances(client,spot_account_id, acc_nickname)
-            main.debug(f'wallet {acc_nickname} state after selling sol (operation number {trades_counter}): '+str(item))
-            # print(item)
+    error_counter = 0
 
-        elif usdt_balance>USDT_MIN_BALANCE_TO_TRADE:
-            main.debug(f'wallet {acc_nickname} has usdt, start buying usdc')
-            volume = buy_usdc(client, spot_account_id, usdt_balance)
-            if volume == 0:
-                error_counter +=1
-                continue
-            actual_turnover += volume
-            trades_counter +=1
-            global_trades_counter+=1
-            time.sleep(1) 
-            item = get_usdt_usdc_sol_balances(client,spot_account_id, acc_nickname)
-            main.debug(f'wallet {acc_nickname} state after buying usdc (operation number {trades_counter}): '+str(item))
-            # print(item)
-        
-        elif usdc_balance>USDT_MIN_BALANCE_TO_TRADE:
-            main.debug(f'wallet {acc_nickname} has usdc, start buying usdt')
-            # print('sell usdc')
-            volume = sell_usdc(client, spot_account_id, usdc_balance)
-            if volume == 0:
-                error_counter +=1
-                continue
-            actual_turnover += volume
-            trades_counter +=1
-            global_trades_counter+=1
-            time.sleep(1) 
-            item = get_usdt_usdc_sol_balances(client,spot_account_id, acc_nickname)
-            main.debug(f'wallet {acc_nickname} state after buying usdt (operation number {trades_counter}): '+str(item))
-            # print(item)
-        
-        else:
-            main.warning(f'wallet {acc_nickname} not enough funds, minimum amounts sol, usdt, usdc: {SOL_MIN_BALANCE_TO_TRADE}, {USDT_MIN_BALANCE_TO_TRADE}, {USDT_MIN_BALANCE_TO_TRADE}')
-            item['turnover']=0
-            item['time']=datetime.now()
-            item['error']=1
-            save_dict_line('operations.csv', item)
-            break
-         
+    while True:
+
+        main.debug(f'actual turnover is: {actual_turnover}')
+
         if actual_turnover>TARGET_TURNOVER:
             main.debug(f'wallet {acc_nickname} has traded {actual_turnover}$, and reached target limit {TARGET_TURNOVER}$ in {trades_counter} trades')
             item['turnover']=actual_turnover
@@ -263,8 +222,86 @@ for index, wallet_key_pair in enumerate(tqdm(keys_list_splitted[:])):
             save_dict_line('operations.csv', item)
             break
 
+
+        assets_list = get_assets_list(client, spot_account_id)
+        usdt_balance, usdc_balance, sol_balance = parse_assets_list(assets_list)
+        
+        item = get_usdt_usdc_sol_balances(client, spot_account_id, acc_nickname)
+        main.debug(f'initial wallet state (balances) {acc_nickname}: '+str(item))
+        # print(item)
+
+        if usdc_balance>USDT_MIN_BALANCE_TO_TRADE and usdt_balance<USDT_MIN_BALANCE_TO_TRADE:
+            main.debug(f'wallet {acc_nickname} has usdc, start buying usdt')
+            # print('sell usdc')
+            trading_amount = usdc_balance
+            if usdc_balance>TARGET_TURNOVER:
+                trading_amount = TARGET_TURNOVER
+            volume = sell_usdc(client, spot_account_id, trading_amount)
+            if volume == 0:
+                error_counter +=1
+                continue
+            # actual_turnover += volume
+            trades_counter +=1
+            global_trades_counter+=1
+            time.sleep(1)
+            item = get_usdt_usdc_sol_balances(client,spot_account_id, acc_nickname)
+            main.debug(f'wallet {acc_nickname} state after buying usdt (operation number {trades_counter}): '+str(item))
+
+        # if usdc_balance>USDT_MIN_BALANCE_TO_TRADE and usdt_balance>USDT_MIN_BALANCE_TO_TRADE:
+
+        elif sol_balance>SOL_MIN_BALANCE_TO_TRADE:
+            main.debug(f'wallet {acc_nickname} has sol, start sell it for usdt')
+            # trading_amount = sol_balance*current_solana_price
+            # if (TARGET_TURNOVER-actual_turnover)<trading_amount:
+            #     trading_amount = TARGET_TURNOVER-actual_turnover
+            # trading_amount = trading_amount/current_solana_price
+
+            volume = sell_sol(client, spot_account_id, sol_balance)
+            if volume == 0:
+                error_counter +=1
+                continue
+            actual_turnover += sol_balance*current_solana_price
+            trades_counter +=1
+            global_trades_counter+=1
+            time.sleep(1)
+            item = get_usdt_usdc_sol_balances(client,spot_account_id, acc_nickname)
+            main.debug(f'wallet {acc_nickname} state after selling sol (operation number {trades_counter}): '+str(item))
+            # print(item)
+
+        elif usdt_balance>USDT_MIN_BALANCE_TO_TRADE:
+            main.debug(f'wallet {acc_nickname} has usdt, start buying sol')
+            trading_amount = usdt_balance
+            if usdt_balance>TARGET_TURNOVER:
+                trading_amount = TARGET_TURNOVER+25
+            if (TARGET_TURNOVER-actual_turnover)<trading_amount:
+                trading_amount = TARGET_TURNOVER-actual_turnover
+            if trading_amount<15:
+                trading_amount = 25
+            # print(trading_amount)
+            volume = buy_sol(client, spot_account_id, trading_amount)
+            if volume == 0:
+                error_counter +=1
+                continue
+            actual_turnover += volume
+            trades_counter +=1
+            global_trades_counter+=1
+            time.sleep(1) 
+            item = get_usdt_usdc_sol_balances(client,spot_account_id, acc_nickname)
+            main.debug(f'wallet {acc_nickname} state after buying sol (operation number {trades_counter}): '+str(item))
+            # print(item)
+        else:
+            main.warning(f'wallet {acc_nickname} not enough funds, minimum amounts sol, usdt, usdc: {SOL_MIN_BALANCE_TO_TRADE}, {USDT_MIN_BALANCE_TO_TRADE}, {USDT_MIN_BALANCE_TO_TRADE}')
+            item['turnover']=0
+            item['time']=datetime.now()
+            item['error']=1
+            save_dict_line('operations.csv', item)
+            break
+         
 elapsed_time = datetime.now()-start_time
-average_trade_processing_duration = elapsed_time/global_trades_counter
+try:
+    average_trade_processing_duration = elapsed_time/global_trades_counter
+except:
+    average_trade_processing_duration = 0
 average_wallet_processing_duration = elapsed_time/len(keys_list_splitted)
 print(f'---------- finished! duration {elapsed_time} for {global_trades_counter} trades in {len(keys_list_splitted)} wallets')
 print(f'average speed is {average_trade_processing_duration} per trade and {average_wallet_processing_duration} per wallet')
